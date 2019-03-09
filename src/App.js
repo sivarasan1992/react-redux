82b197a99119602cd35881d9af16f6b161b04b47
sivarasan1992 <sivarasan@technohive.tech>
2019-03-10 00:08:11 +0530

    added redux with react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+import { startAction } from "actions/startAction";
+import { stopAction } from "actions/stopAction";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            src={logo}
+            className={
+              "App-logo" + (this.props.rotating ? "" : " App-logo-paused")
+            }
+            alt="logo"
+            onClick={
+              this.props.rotating
+                ? this.props.stopAction
+                : this.props.startAction
+            }
+          />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -24,5 +38,15 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  startAction: () => dispatch(startAction),
+  stopAction: () => dispatch(stopAction)
+});
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
